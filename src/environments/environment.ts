a1ed19ab1d796737969b5dbf59206940e3020aa4
Param <parampreet@digisoftsolution.com>
2026-04-09 15:44:37 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: "https://api.peacemappers.ai",
   //apiUrl: 'https://localhost:63334',
-  subscriptionUrl:'https://veridianurbansystems.com',
+  subscriptionUrl:'https://peacemappers.ai',
   googleClientId: '419053098195-ink9t3quhqbfc0li446rn1qf9tci4hm3.apps.googleusercontent.com',
   facebookAppId: '1784178838961034',
   stripePublicKey: 'pk_test_VhPtypJ0veVOYXRHer1kazHJ',
